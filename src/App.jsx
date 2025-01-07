4cd1733a73927e3c03e4d2978169352cb00edfb4
Artem Zhuravkov <navy2494@gmail.com>
2025-01-07 16:24:50 +0200

    upd settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistrationForm from "./components/registration-form/RegistrationForm";
 import AuthorizationForm from "./components/authorization-from/AuthorizationForm";
 import { Home, SendEmail } from "./pages";
@@ -6,12 +6,14 @@ import "./App.css";
 
 function App() {
   return (
+    <Router>
       <Routes>
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<AuthorizationForm />} />
         <Route path="/" element={<Home />} />
         <Route path="/send-email" element={<SendEmail />} />
       </Routes>
+    </Router>
   );
 }
 
